64df86c1688a98e339d556c0654942f49d87d32b
Ben Moore <b3nmoore@gmail.com>
2014-06-07 14:06:55 +0100

    let's see

--- javascripts/action_gross.js
+++ javascripts/action_gross.js
@@ -4,13 +4,21 @@ d3.json("javascripts/json/action.json", function(error, json) {
   if (error) return console.warn(error);
   data = json;
 
+  if($(window).width() < 700){
+    width = $(window).width()/1.3;
+  } else {
+    width = 600;
+  }
+
+
   // width then height
-  var svg = dimple.newSvg("#actionGross", 520, 700);
+  var svg = dimple.newSvg("#actionGross", 550, 700);
+  console.log($("#wrapper").width());
 
   var myChart = new dimple.chart(svg, data);
-//margins : outside plot area, bounds inside
-//  myChart.setMargins("20%", "5%", "5%", "20%");
-  myChart.setBounds("18%", "2%", "75%", "80%");
+  //margins : outside plot area, bounds inside
+  //  myChart.setMargins("20%", "5%", "5%", "20%");
+  myChart.setBounds("20%", "2%", "75%", "80%");
 
   //x axis
   var x = myChart.addCategoryAxis("x", "actor");
