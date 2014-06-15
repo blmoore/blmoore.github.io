64825ee8f2a84daa48a86b86bfc3b8363d1753d8
Ben Moore <b3nmoore@gmail.com>
2014-06-15 20:36:15 +0100

    test _site add

---
+++ blog2/js/action_budget.js
@@ -0,0 +1,41 @@
+d3.json("/blog/js/json/action.json", function(error, json) {
+  if (error) return console.warn(error);
+  data = json;
+
+  if($(window).width() < 700){
+    width = $(window).width()/1.3;
+    height = width * 1.2;
+  } else {
+    width = 550;
+    height = 700;
+  }
+
+  // width then height
+  var svg = dimple.newSvg("#actionBudget", width, height);
+
+  var myChart = new dimple.chart(svg, data);
+  //margins : outside plot area, bounds inside
+  //  myChart.setMargins("20%", "5%", "5%", "20%");
+  myChart.setBounds("20%", "2%", "72%", "80%");
+  //x axis
+  var x = myChart.addCategoryAxis("x", "actor");
+  x.title = null;
+
+  //y axis
+  var commasFormatter = d3.format(",.0f");
+  var y = myChart.addMeasureAxis("y", "Adjusted budget");
+
+  y.tickFormat = ",.f";
+  y.title = "Budget (2014 US dollars)";
+
+
+  var s = myChart.addSeries(["actor", "film"], dimple.plot.bar, [x, y]);
+
+  myChart.draw();
+
+  // thanks to http://stackoverflow.com/a/17792468/1274516
+  x.shapes.selectAll("text").attr("transform",
+  function (d) {
+    return d3.select(this).attr("transform") + " translate(0, 20) rotate(-45)";
+  });
+});
---
+++ blog2/js/action_budgetVgross.js
@@ -0,0 +1,35 @@
+d3.json("/blog/js/json/action.json", function(error, json) {
+  if (error) return console.warn(error);
+  data = json;
+
+  if($(window).width() < 700){
+    width = $(window).width()/1.3;
+  } else {
+    width = 600;
+  }
+
+  var svg = dimple.newSvg("#actionBudgetVGross", width, width);
+
+  var myChart = new dimple.chart(svg, data);
+  myChart.setMargins("10%", "5%", "5%", "10%");
+
+  //x axis
+  var x = myChart.addMeasureAxis("x", "Adjusted budget");
+  x.title = "Adjusted budget (2014 US dollars)";
+
+
+  //y axis
+  var y = myChart.addMeasureAxis("y", "Adjusted gross");
+
+//  y.tickFormat = ",.f";
+  y.title = "Estimated gross (2014 US dollars)";
+
+
+  var s = myChart.addSeries(["actor", "film"], dimple.plot.bubble, [x, y]);
+
+  myChart.draw();
+
+//ccould force reload on window size change, but jittery
+//  $(window).resize(function(){location.reload();});
+
+});
